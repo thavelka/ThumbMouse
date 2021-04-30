84e1743fcb8e56e5610ba257a31740a4c25f30d8
Tim Havelka <thavelka@users.noreply.github.com>
2021-04-29 21:28:58 -0500

    Initial commit

---
+++ mouse/mouse.ino
@@ -0,0 +1,80 @@
+#include<Mouse.h>
+
+#define L_CLICK 2
+#define R_CLICK 3
+#define STICK_CLICK 4
+#define STICK_X A0
+#define STICK_Y A1
+
+const int sensitivity = 50;
+int xCenter;
+int yCenter;
+bool lClicked = false;
+bool rClicked = false;
+bool sClicked = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Mouse.begin();
+  pinMode(L_CLICK, INPUT_PULLUP);
+  pinMode(R_CLICK, INPUT_PULLUP);
+  pinMode(STICK_CLICK, INPUT_PULLUP);
+  pinMode(STICK_X, INPUT);
+  pinMode(STICK_Y, INPUT);
+
+  xCenter = analogRead(STICK_X);
+  yCenter = analogRead(STICK_Y);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int xVal = analogRead(STICK_X) - xCenter;
+  int yVal = analogRead(STICK_Y) - yCenter;
+  bool lClick = !digitalRead(L_CLICK);
+  bool rClick = !digitalRead(R_CLICK);
+  bool sClick = !digitalRead(STICK_CLICK);
+
+  if (xVal > 20) {
+    xVal = 1;
+  } else if (xVal < -20) {
+    xVal = -1;
+  } else {
+    xVal = 0;
+  }
+
+  if (yVal > 20) {
+    yVal = 1;
+  } else if (yVal < -20) {
+    yVal = -1;
+  } else {
+    yVal = 0;
+  }
+
+  if (xVal != 0 || yVal != 0) {
+    Mouse.move(xVal, yVal, 0);
+  }
+ 
+  if (lClick && !lClicked) {
+    Mouse.press(MOUSE_LEFT);
+    lClicked = true;
+  } else if (!lClick && lClicked) {
+    Mouse.release(MOUSE_LEFT);
+    lClicked = false;
+  }
+  if (rClick && !rClicked) {
+    Mouse.press(MOUSE_RIGHT);
+    rClicked = true;
+  } else if (!rClick && rClicked) {
+    Mouse.release(MOUSE_RIGHT);
+    rClicked = false;
+  }
+  if (sClick && !sClicked) {
+    Mouse.press(MOUSE_LEFT);
+    sClicked = true;
+  } else if (!sClick && sClicked) {
+    Mouse.release(MOUSE_LEFT);
+    sClicked = false;
+  }
+
+  delay(2);
+}
